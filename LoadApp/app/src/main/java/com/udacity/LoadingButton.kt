7f7424bf897df6ab87726768ea0f6447109e6ef2
toufeeqahamedns <toufeeq.nas@gmail.com>
2021-01-17 19:15:47 +0530

    Adding downloading feature with custom button animations

--- LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
+++ LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,11 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat.getColor
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -12,22 +15,112 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var progressWidth = 0
+    private var angle = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var buttonBackgroundColor = 0
+    private var textColor = 0
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var rectangleAnimator = ValueAnimator()
+    private var circleAnimator = ValueAnimator()
 
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when (new) {
+            ButtonState.Loading -> {
+                rectangleAnimator = ValueAnimator.ofInt(0, widthSize)
+                rectangleAnimator.apply {
+                    duration = 2000
+                    addUpdateListener { valueAnimator ->
+                        progressWidth = animatedValue as Int
+                        valueAnimator.repeatCount = ValueAnimator.INFINITE
+                        valueAnimator.repeatMode = ValueAnimator.REVERSE
+                        invalidate()
+                    }
+                    start()
+                }
+                circleAnimator = ValueAnimator.ofInt(0, 360)
+                circleAnimator.apply {
+                    duration = 2000
+                    addUpdateListener { valueAnimator ->
+                        angle = valueAnimator.animatedValue as Int
+                        valueAnimator.repeatCount = ValueAnimator.INFINITE
+                        invalidate()
+                    }
+                    start()
+                }
+            }
+            ButtonState.Completed -> {
+                rectangleAnimator.end()
+                progressWidth = 0
+                circleAnimator.end()
+                angle = 0
+                invalidate()
+            }
+        }
     }
 
 
     init {
-
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.LoadingButton,
+            0,
+            0
+        ).apply {
+            try {
+                buttonBackgroundColor = getColor(
+                    R.styleable.LoadingButton_buttonBackgroundColor,
+                    context.getColor(R.color.colorPrimary)
+                )
+                textColor = getColor(R.styleable.LoadingButton_textColor, Color.WHITE)
+            } finally {
+                recycle()
+            }
+        }
     }
 
+    private val paint = Paint()
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        // drawing the rectangle
+        paint.color = buttonBackgroundColor
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        // drawing the progress rectangle
+        paint.color = getColor(context, R.color.colorPrimaryDark)
+        canvas?.drawRect(
+            0f,
+            0f,
+            widthSize.toFloat() * progressWidth / 100,
+            heightSize.toFloat(),
+            paint
+        )
+
+        // drawing the text
+        paint.color = textColor
+        paint.textSize = 60f
+        paint.textAlign = Paint.Align.CENTER
+        val buttonLabel = when (buttonState) {
+            ButtonState.Clicked -> "Clicked"
+            ButtonState.Loading -> "We are loading"
+            ButtonState.Completed -> "Download"
+        }
+        canvas?.drawText(buttonLabel, (widthSize / 2).toFloat(), (heightSize / 1.65).toFloat(), paint)
+
+        // drawing the circle
+        paint.color = Color.YELLOW
+        canvas?.drawArc(
+            (widthSize - 100f),
+            (heightSize / 2) - 50f,
+            (widthSize - 50f),
+            (heightSize / 2) + 50f,
+            0F,
+            angle.toFloat(),
+            true,
+            paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,4 +136,10 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        invalidate()
+        return true
+    }
 }
--- LoadApp/app/src/main/java/com/udacity/MainActivity.kt
+++ LoadApp/app/src/main/java/com/udacity/MainActivity.kt
@@ -7,17 +7,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import javax.microedition.khronos.opengles.GL
 
 
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private lateinit var downloadManager: DownloadManager
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -31,33 +35,62 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.glide_radio_button -> download(GLIDE_URL)
+                R.id.project_radio_button -> download(PROJECT_URL)
+                R.id.retrofit_radio_button -> download(RETROFIT_URL)
+                else -> Toast.makeText(
+                    this,
+                    "Please select the file to download",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            if (downloadID == id) {
+                Toast.makeText(context, "Download Complete", Toast.LENGTH_SHORT).show()
+            }
+
+            val cursor: Cursor = downloadManager.query(DownloadManager.Query().setFilterById(downloadID))
+            while (cursor.moveToNext()) {
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                when (status) {
+                    DownloadManager.STATUS_FAILED -> {
+                        custom_button.buttonState = ButtonState.Completed
+                    }
+                    DownloadManager.STATUS_SUCCESSFUL -> {
+                        custom_button.buttonState = ButtonState.Completed
+                    }
+                }
+            }
         }
     }
 
-    private fun download() {
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        custom_button.buttonState = ButtonState.Loading
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
-        private const val URL =
+        private const val PROJECT_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/master.zip"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
